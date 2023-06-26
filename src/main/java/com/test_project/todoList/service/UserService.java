f90d2804fbddda693ff1b88c9641443b5d5c2761
lee93 <dohuny2@naver.com>
2023-06-27 00:21:05 +0900

    2023-06-27
    User Layer 구현
    - UserEntity
    - UserRepository
    - UserService
    - UserDTO / UserController

---
+++ src/main/java/com/test_project/todoList/service/UserService.java
@@ -0,0 +1,32 @@
+package com.test_project.todoList.service;
+
+import com.test_project.todoList.model.UserEntity;
+import com.test_project.todoList.persistence.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Slf4j
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserEntity create(final UserEntity userEntity) {
+        if (userEntity == null || userEntity.getEmail() == null) {
+            throw new RuntimeException("Invalid arguments");
+        }
+        final String email = userEntity.getEmail();
+        if(userRepository.existsByEmail(email)){
+            log.warn("Email already exists {}", email);
+            throw new RuntimeException("Email already exists");
+        }
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity getByCredentials(final String email, final String password) {
+        return userRepository.findByEmailAndPassword(email, password);
+    }
+}
