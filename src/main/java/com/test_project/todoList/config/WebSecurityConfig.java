eedc154568e7c92db70f8cb58fbc9f940627ece2
lee93 <dohuny2@naver.com>
2023-06-27 23:20:36 +0900

    2023-06-27 Spring Security 설정 ( ..ing) WebSecurityConfigurerAdapter Deprecated 문제 해결

--- src/main/java/com/test_project/todoList/config/WebSecurityConfig.java
+++ src/main/java/com/test_project/todoList/config/WebSecurityConfig.java
@@ -2,42 +2,41 @@ package com.test_project.todoList.config;
 
 import com.test_project.todoList.security.JwtAuthenticationFilter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 @EnableWebSecurity
-@Slf4j
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
+
     @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @Override
-    protected void configure (HttpSecurity http) throws Exception{
-        // http 시큐리티 빌더
-        http.cors() // WebMvcConfig 에서 이미 설정했으므로 기본 cors 설정
-                .and()
-                .csrf() // csrf 는 현재 사용하지 않으므로 disable
-                .disable()
-                .httpBasic()    // token 을 사용하므로 basic 인증 disable
-                .disable()
-                .sessionManagement()    // session 기반이 아님을 선언
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeRequests()     // /와 /auth/** 경로는 인증 안해도 됨
-                .antMatchers("/","/auth/**").permitAll()
-                .anyRequest()   //  /와 /auth/** 이외의 모든 경로는 인증해야 됨
-                .authenticated();
-        // filter 등록
-        // 매 요청마다
-        // CorsFilter 실행한 후에
-        // jwtAuthenticationFilter 실행한다
-        http.addFilterAfter(
-                jwtAuthenticationFilter,
-                CorsFilter.class
-        );
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(cors -> cors.disable())
+                .csrf(csrf ->
+                        csrf.disable())
+                .httpBasic(basic ->
+                        basic.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/","/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
+
+        http.addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+
+        return http.getOrBuild();
     }
+
 
 }
